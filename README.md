0f4b80ec5d39e7f56ad486d1b7de772bc69c7cc7
okdistribute <633012+okdistribute@users.noreply.github.com>
2020-08-28 14:49:24 -0700

    feat: add readme & hidden-mapbox

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # hidden-mapbox
-A utility wrapper for mapbox-gl to do operations while hidden
+
+A utility wrapper for mapbox-gl to do operations while hidden.
+
+## Usage
+
+```js
+const HiddenMapbox = require('hidden-mapbox')
+
+const hmap = new HiddenMapbox({
+  accessToken, style
+})
+
+const image = await hmap.getMapImage({
+  width, height, center, zoom, pixelRatio
+})
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,68 @@
+var mapboxgl = require('mapbox-gl')
+const originalPixelRatio = window.devicePixelRatio
+
+class Map {
+  constructor (props) {
+    if (!props) props = {}
+    this.mapDiv = this._createMapDiv()
+    if (props.accessToken) mapboxgl.accessToken = props.accessToken
+    this.map = new mapboxgl.Map({
+      container: this.mapDiv,
+      preserveDrawingBuffer: true,
+      style: props.style
+    })
+  }
+
+  /**
+   * Returns a hidden div element for the map
+   * @returns {HTMLElement}
+   */
+  _createMapDiv () {
+    const mapDiv = document.createElement('div')
+    document.body.appendChild(mapDiv)
+    mapDiv.style.visibility = 'hidden'
+    mapDiv.style.position = 'absolute'
+    mapDiv.style.width = '500px'
+    mapDiv.style.height = '500px'
+    return mapDiv
+  }
+
+  setStyle (style) {
+    this.map.setStyle(style)
+  }
+
+  /**
+   * @param {import('./types').MapOptions} mapOptions
+   * @returns {Promise<Blob?>}
+   */
+  async getMapImage ({
+    accessToken,
+    width,
+    height,
+    style,
+    center,
+    zoom,
+    pixelRatio
+  }) {
+    return new Promise((resolve) => {
+      if (accessToken) mapboxgl.accessToken = accessToken
+      if (style) this.setStyle(style)
+      this.mapDiv.style.width = width + 'px'
+      this.mapDiv.style.height = height + 'px'
+      // @ts-ignore This is actually writable in Chrome, and can be used to
+      // trick Mapbox into rendering higher-res
+      window.devicePixelRatio = pixelRatio
+      this.map.resize()
+      this.map.jumpTo({ center, zoom })
+      this.map.on('idle', () => {
+        // @ts-ignore
+        window.devicePixelRatio = originalPixelRatio
+        this.map.getCanvas().toBlob((blob) => {
+          resolve(blob)
+        })
+      })
+    })
+  }
+}
+
+module.exports = Map
